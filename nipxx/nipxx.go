964b756df5e771adec482b9e2e7b949c8cb4f4e6
Braydon Fuller <courier@braydon.com>
2024-09-03 16:39:04 -0700

    Add recovery key event and start validation.

--- nipxx/nipxx.go
+++ nipxx/nipxx.go
@@ -2,6 +2,9 @@ package nipxx
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"github.com/nbd-wtf/go-nostr"
 )
 
 const (
@@ -50,20 +53,86 @@ type RecoveryKeysAttestation struct {
 	RecoveryKeysSetup string
 }
 
+func MakeRecoveryKeysSetupEvent(setup *RecoveryKeysSetup, createdAt nostr.Timestamp) *nostr.Event {
+	evt := nostr.Event{}
+	evt.CreatedAt = createdAt
+	evt.Kind = KindRecoveryKeysSetup
+
+	evt.Tags = make([]nostr.Tag, 0, len(setup.RecoveryPubKeys) + 2)
+
+	for _, pubkey := range setup.RecoveryPubKeys {
+		evt.Tags = evt.Tags.AppendUnique(nostr.Tag{"p", pubkey})
+	}
+
+	evt.Tags = evt.Tags.AppendUnique(nostr.Tag{"threshold", fmt.Sprintf("%d", setup.Threshold)})
+	evt.Tags = evt.Tags.AppendUnique(nostr.Tag{SafeguardRecoveryKeysSetup})
+
+	evt.Content = setup.Comment
+
+	return &evt
+}
+
 func MakeRecoveryKeysSetup(
 	pubkeys [] string,
 	threshold int,
 	comment string,
-) (error, *RecoveryKeysSetup) {
-	 if threshold == 0 {
-		 return errors.New("Threshold must not be zero."), nil
-	 }
-
+) *RecoveryKeysSetup {
 	setup := &RecoveryKeysSetup{
 		RecoveryPubKeys: pubkeys,
 		Threshold: threshold,
 		Comment: comment,
 	}
 
-	return nil, setup
+	return setup
+}
+
+func ValidateRecoveryKeysEvent(evt *nostr.Event) error {
+	// Check the kind.
+	if evt.Kind != KindRecoveryKeysSetup {
+		return errors.New("Invalid kind.")
+	}
+
+	// Check the safeguard tag to verify that it has
+	// been included correctly.
+	safeguard := evt.Tags.GetAll([]string{SafeguardRecoveryKeysSetup})
+
+	if len(safeguard) != 1 {
+		return errors.New("Must include one safeguard tag.")
+	}
+
+	if len(safeguard[0]) != 1 {
+		return errors.New("Safeguard must not include a value.")
+	}
+
+	// Check that the threshold tag has been included
+	// and has a valid value.
+	thresholds := evt.Tags.GetAll([]string{"threshold"})
+
+	if len(thresholds) != 1 {
+		return errors.New("Must include one threshold tag.")
+	}
+
+	if len(thresholds[0]) != 2 {
+		return errors.New("Threshold tag must include one value.")
+	}
+
+	threshold, err := strconv.Atoi(thresholds[0][1])
+
+	if err != nil {
+		return errors.New("Threshold tag value must be an integer.")
+	}
+
+	if threshold <= 0 {
+		return errors.New("Threshold tag value must be a non-zero positive integer.")
+	}
+
+	// Check that, at a minimum, one recovery pubkey has been included
+	// and that it has a valid value.
+	pubkeys := evt.Tags.GetAll([]string{"p"})
+
+	if len(pubkeys) < 1 {
+		return errors.New("Must include one or more recovery pubkeys")
+	}
+
+	return nil
 }
--- nipxx/nipxx_test.go
+++ nipxx/nipxx_test.go
@@ -2,7 +2,9 @@ package nipxx
 
 import (
 	"testing"
+	"encoding/json"
 	"github.com/stretchr/testify/assert"
+	"github.com/nbd-wtf/go-nostr"
 )
 
 func TestMakeRecoveryKeysSetup(t *testing.T) {
@@ -10,21 +12,48 @@ func TestMakeRecoveryKeysSetup(t *testing.T) {
 		PubKeys []string
 		Threshold  int
 		Comment string
-		Error string
 	}{
-		{[]string{"4fe17162aa42c96d7757f98cabc8a0b38ceb61a9160195b5d16bce6f6d8064ca", "8b57adf363f3abed31ea6e0b664884af07e2a92611154599345f6a63f9c70f02", "741a0fb3d23db2c87f82a9a979084893c3f094c47776c1283dd313331fc4b308"}, 2, "Setting up my first set of recovery keys! Yay!", ""},
-		{[]string{"4fe17162aa42c96d7757f98cabc8a0b38ceb61a9160195b5d16bce6f6d8064ca", "8b57adf363f3abed31ea6e0b664884af07e2a92611154599345f6a63f9c70f02", "741a0fb3d23db2c87f82a9a979084893c3f094c47776c1283dd313331fc4b308"}, 0, "Doing something illogical!", "Threshold must not be zero."},
+		{[]string{"4fe17162aa42c96d7757f98cabc8a0b38ceb61a9160195b5d16bce6f6d8064ca", "8b57adf363f3abed31ea6e0b664884af07e2a92611154599345f6a63f9c70f02", "741a0fb3d23db2c87f82a9a979084893c3f094c47776c1283dd313331fc4b308"}, 2, "Setting up my first set of recovery keys! Yay!"},
+		{[]string{"4fe17162aa42c96d7757f98cabc8a0b38ceb61a9160195b5d16bce6f6d8064ca", "8b57adf363f3abed31ea6e0b664884af07e2a92611154599345f6a63f9c70f02", "741a0fb3d23db2c87f82a9a979084893c3f094c47776c1283dd313331fc4b308"}, 0, "Doing something illogical!"},
 	} {
-		err, setup := MakeRecoveryKeysSetup(vector.PubKeys, vector.Threshold, vector.Comment)
+		setup := MakeRecoveryKeysSetup(vector.PubKeys, vector.Threshold, vector.Comment)
 
-		if vector.Error == "" {
-			assert.Nil(t, err)
-			assert.Equal(t, setup.Comment, vector.Comment)
-			assert.Equal(t, setup.RecoveryPubKeys, vector.PubKeys)
-			assert.Equal(t, setup.Threshold, vector.Threshold)
-		} else {
-			assert.NotNil(t, err)
-			assert.Errorf(t, err, vector.Error, err)
-		}
+		assert.Equal(t, setup.Comment, vector.Comment)
+		assert.Equal(t, setup.RecoveryPubKeys, vector.PubKeys)
+		assert.Equal(t, setup.Threshold, vector.Threshold)
+	}
+}
+
+func TestMakeRecoveryKeysSetupEvent(t *testing.T) {
+	for _, vector := range []struct {
+		PrivateKey string
+		Timestamp int64
+		PubKeys []string
+		Threshold  int
+		Comment string
+		JSON string
+	}{
+		{
+			"ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
+			int64(1725402764),
+			[]string{
+				"4fe17162aa42c96d7757f98cabc8a0b38ceb61a9160195b5d16bce6f6d8064ca",
+				"8b57adf363f3abed31ea6e0b664884af07e2a92611154599345f6a63f9c70f02",
+				"741a0fb3d23db2c87f82a9a979084893c3f094c47776c1283dd313331fc4b308",
+			},
+			2,
+			"Setting up my first set of recovery keys! Yay!",
+			"{\"kind\":51,\"id\":\"40808ce2685b0aeaa756d05fc99853db0c79043210a3a949fdbc905ea2984c05\",\"pubkey\":\"9166c289b9f905e55f9e3df9f69d7f356b4a22095f894f4715714aa4b56606af\",\"created_at\":1725402764,\"tags\":[[\"p\",\"4fe17162aa42c96d7757f98cabc8a0b38ceb61a9160195b5d16bce6f6d8064ca\"],[\"p\",\"8b57adf363f3abed31ea6e0b664884af07e2a92611154599345f6a63f9c70f02\"],[\"p\",\"741a0fb3d23db2c87f82a9a979084893c3f094c47776c1283dd313331fc4b308\"],[\"threshold\",\"2\"],[\"recovery-keys-setup\"]],\"content\":\"Setting up my first set of recovery keys! Yay!\",\"sig\":\"8e73482ffae5261edb5181960a8d50974de468f20ea9f505cb57812e348ee920468dbb390ab39b497e33ac9aa2e6f784305bea831b232585171ab0878c04c4b3\"}",
+		},
+	} {
+		setup := MakeRecoveryKeysSetup(vector.PubKeys, vector.Threshold, vector.Comment)
+		evt := MakeRecoveryKeysSetupEvent(setup, nostr.Timestamp(vector.Timestamp))
+
+		evt.Sign(vector.PrivateKey)
+
+		evtjson, err := json.Marshal(evt)
+
+		assert.Nil(t, err)
+		assert.Equal(t, vector.JSON, string(evtjson))
 	}
 }
